58d9cf2d4f3bfcdbb5f3ea2dbf974b7a93a2351f
Miltos T <miltosdev@gmail.com>
2023-07-18 16:01:11 +0300

    add script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,16 @@
 def buildApp() {
-    echo "building the app with mvn clean package..."
+    echo "building the app..."
+    // mvn clean package to delete older jars
+    // Dockerfile builds with regexp(*) so it needs one jar only
     sh "mvn clean package"
 }
 
 def testApp() {
     echo "testing the app..."
-    echo "groovy script testApp() debug echo"
 }
 
 def deployApp() {
-    echo "deplloying the app..."    
+    echo "deploying the app..."    
 }
 
 return this
